17b145babefbd77ccf49fbb47b6a809ae6d3e826
Karlo Ivic <ivickarlo2000@gmail.com>
2022-11-29 16:56:44 +0100

    AB#39
    Kategorije su dodane kao demo izgled, i okruzenje za kategorije su implementirane. U buducnosti cemo update jos, ovo je za prvi sprint dovoljno.

--- frontend/src/app-routing.components.ts
+++ frontend/src/app-routing.components.ts
@@ -5,7 +5,7 @@ import { CategoriesComponent } from "./app/categories/categories.component";
 import { LoginComponent } from "./app/login/login.component";
 import { SignUpComponent } from "./app/sign-up/sign-up.component";
 import { SlideComponent } from "./app/slide/slide.component";
-import { MainPageComponent } from "./app/user-page/main-page.component";
+import { MainPageComponent } from "./app/main-page/main-page.component";
 
 const routes: Routes = [
     {path:"",component: SlideComponent},
